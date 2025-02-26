0be2fb80bf377981276b8098cbcf7e2542fc522d
JJ-in-Jersey <jason.bronfeld@pm.me>
2025-02-26 16:35:03 -0500

    moved rise and set angles to left most columns

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from tt_os_abstraction.os_abstraction import env
 if __name__ == '__main__':
 
     frame_path = env('user_profile').joinpath('Fair Currents/sunrise-sunset.csv')
-    frame = pd.DataFrame(columns=['date', 'fracillum', 'Begin Civil Twilight', 'Rise',  'End Golden Hour', 'Begin Golden Hour', 'Set', 'End Civil Twilight'])
+    frame = pd.DataFrame(columns=['date', 'RISE degrees', 'SET degrees', 'fracillum', 'fracillum index', 'Begin Civil Twilight', 'Rise',  'End Golden Hour', 'Begin Golden Hour', 'Set', 'End Civil Twilight'])
 
     start = dt(2024, 12, 1)
     end = dt(2026, 1, 31)
@@ -24,7 +24,7 @@ if __name__ == '__main__':
             response = requests.get(request_string)
             response.raise_for_status()
             response_dict = json.loads(response.text)
-            fracillum = response_dict['properties']['data']['fracillum']
+            fracillum = int(response_dict['properties']['data']['fracillum'][:-1])
             sd = response_dict['properties']['data']['sundata']
             frame_dict = {'date': date, 'fracillum': fracillum, sd[0]['phen']: sd[0]['time'].split(' ')[0] + ':00', sd[1]['phen']: sd[1]['time'].split(' ')[0] + ':00', sd[3]['phen']: sd[3]['time'].split(' ')[0] + ':00', sd[4]['phen']: sd[4]['time'].split(' ')[0] + ':00'}
             frame.loc[len(frame)] = frame_dict
@@ -32,8 +32,7 @@ if __name__ == '__main__':
             print(e)
         date = date + td(days=1)
 
-
-
+    frame['fracillum index'] = frame.fracillum.apply(lambda x: int(x/5))
     frame['BCT degrees'] = frame['Begin Civil Twilight'].apply(time_to_degrees)
     frame['RISE degrees'] = frame['Rise'].apply(time_to_degrees)
     frame['End Golden Hour'] = pd.to_datetime(frame['Begin Civil Twilight']) + pd.Timedelta(hours=1)
